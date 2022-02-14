12a9a7047883a7069a576b563eb9740dbbcec60a
Lucas Dantas <lucasdantasmoura01@gmail.com>
2022-02-14 11:20:36 -0300

    Template, styles e fonts

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View } from 'react-native'
+
+import {
+  Container,
+ // Title
+} from './styles';
+
+export function Home(){
+  return (
+    <Container>
+        {/* <Title>
+
+        </Title> */}
+    </Container>
+  );
+}
